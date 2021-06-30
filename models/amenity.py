54c748b2ee79e377af92beed45cbea00bc0d4a40
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-30 18:23:00 -0500

    Add new clases

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+........
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    ....
+    """
+
+    name = ""
--- models/main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-bm = BaseModel()
