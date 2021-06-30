02f1ad813a8d0d6eff9b6c68c43bcb83cf9fe0df
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-30 14:21:02 -0500

    Fix BaseMode

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import models
 import uuid
 
-formt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
@@ -27,9 +26,9 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
-            models.storage.save()
+            
 
     def __str__(self):
         """String representation of the BaseModel class"""
